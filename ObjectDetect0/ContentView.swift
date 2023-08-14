73166384262a587d0b4cc9396056a4feb1a668db
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-08-14 15:23:22 -0400

    debug working measure on wrist position

--- ObjectDetect0/ContentView.swift
+++ ObjectDetect0/ContentView.swift
@@ -20,39 +20,91 @@ struct CameraView: UIViewControllerRepresentable {
     }
 }
 struct ContentView: View {
+    @State private var selectedOption = 0
     @State private var isTwoHandsDetected = false
+    @State private var isVictoryPoseDetected = false
+    
+    var menuTitle: String {
+        switch selectedOption {
+        case 0:
+            return "Two Hands"
+        case 1:
+            return "Victory Pose"
+        case 2:
+            return "Option 3"
+        default:
+            return "Choose Hand Pose"
+        }
+    }
+
 
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            VStack {
-                Image("abe")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Spacer()
-            }
-            .padding()
-
-            if isTwoHandsDetected {
-                Rectangle()
-                    .fill(Color.clear)
-                    
-            } else {
-                Rectangle()
-                    .fill(Color.black)
-            }
-            
-            Spacer()
-            
-            CameraView()
-                .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 4)
-                .background(isTwoHandsDetected ? Color.clear : Color.black)
-                .cornerRadius(20)
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.green, lineWidth: isTwoHandsDetected ? 5 : 0))
+        
+        ZStack{
+            ZStack(alignment: .bottomTrailing) {
+                VStack {
+                    Image("abe")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Spacer()
+                }
                 .padding()
-        }
-        .onReceive(NotificationCenter.default.publisher(for: .numberOfHandsDetectedChanged)) { notification in
-            if let numberOfHands = notification.object as? Int {
-                isTwoHandsDetected = numberOfHands == 2
+                
+                
+                if isTwoHandsDetected {
+                    Rectangle()
+                        .fill(Color.clear)
+                    
+                } else {
+                    Rectangle()
+                        .fill(Color.black)
+                }
+                
+                Spacer()
+                
+                CameraView()
+                    .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 4)
+                    .background((selectedOption == 0 && isTwoHandsDetected) || (selectedOption == 1 && isVictoryPoseDetected) ? Color.clear : Color.black)
+                    .cornerRadius(20)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.green, lineWidth: (selectedOption == 0 && isTwoHandsDetected) || (selectedOption == 1 && isVictoryPoseDetected) ? 5 : 0))
+                    .padding()
+
+            } //edit here
+            .onReceive(NotificationCenter.default.publisher(for: .numberOfHandsDetectedChanged)) { notification in
+                if selectedOption == 0, let numberOfHands = notification.object as? Int {
+                    isTwoHandsDetected = numberOfHands == 2
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .numberOfVictoryHandsDetectedChanged)) { notification in
+                if selectedOption == 1, let numberOfVictoryHands = notification.object as? Int {
+                    isVictoryPoseDetected = numberOfVictoryHands == 2
+                }
+            }
+
+            
+            VStack {
+                           
+                Menu(menuTitle) {
+                    Button(action: { selectedOption = 0 }) {
+                        Text("Two Hands")
+                            .font(.system(size: 18, weight: .bold)) // Larger and bold text
+                    }
+                    Button(action: { selectedOption = 1 }) {
+                        Text("Victory Pose")
+                            .font(.system(size: 18, weight: .bold)) // Larger and bold text
+                    }
+                    Button(action: { selectedOption = 2 }) {
+                        Text("Option 3")
+                            .font(.system(size: 18, weight: .bold)) // Larger and bold text
+                    }
+                }
+
+                .padding()
+                .foregroundColor(Color.black)
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.top, 20)
+                Spacer()
             }
         }
     }
@@ -62,6 +114,11 @@ struct ContentView: View {
 extension Notification.Name {
     static let numberOfHandsDetectedChanged = Notification.Name("numberOfHandsDetectedChanged")
 }
+
+extension NSNotification.Name {
+    static let numberOfVictoryHandsDetectedChanged = NSNotification.Name("numberOfVictoryHandsDetectedChanged")
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- ObjectDetect0/ViewController.swift
+++ ObjectDetect0/ViewController.swift
@@ -72,14 +72,25 @@ class ViewController: UIViewController {
     connection.videoOrientation = .portrait
       
   }
-
+    
+  
     private func detectHands(image: CVPixelBuffer) {
         let handPoseRequest = VNDetectHumanHandPoseRequest { [weak self] vnRequest, error in
             DispatchQueue.main.async {
                 guard let self = self else { return }
                 if let results = vnRequest.results as? [VNHumanHandPoseObservation], !results.isEmpty {
-                    let numberOfHands = results.count
-                    NotificationCenter.default.post(name: .numberOfHandsDetectedChanged, object: numberOfHands)
+                    var victoryHandsCount = 0
+                    for observation in results {
+                        if self.isHandInVictoryPosition(observation: observation) {
+                            victoryHandsCount += 1
+                        }
+                    }
+
+                    // Post the number of victory hands detected
+                    NotificationCenter.default.post(name: .numberOfVictoryHandsDetectedChanged, object: victoryHandsCount)
+
+                    // Post total number of hands detected
+                    NotificationCenter.default.post(name: .numberOfHandsDetectedChanged, object: results.count)
                 } else {
                     NotificationCenter.default.post(name: .numberOfHandsDetectedChanged, object: 0)
                 }
@@ -90,6 +101,56 @@ class ViewController: UIViewController {
 
         let imageResultHandler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
         try? imageResultHandler.perform([handPoseRequest])
+    }
+    
+    private func distance(from point1: CGPoint, to point2: CGPoint) -> CGFloat {
+        return sqrt(pow(point2.x - point1.x, 2) + pow(point2.y - point1.y, 2))
+    }
+
+    private func isHandInVictoryPosition(observation: VNHumanHandPoseObservation) -> Bool {
+        guard let thumbPoints = try? observation.recognizedPoints(.thumb),
+              let indexPoints = try? observation.recognizedPoints(.indexFinger),
+              let middlePoints = try? observation.recognizedPoints(.middleFinger),
+              let ringPoints = try? observation.recognizedPoints(.ringFinger),
+              let littlePoints = try? observation.recognizedPoints(.littleFinger),
+              let wristPoint = try? observation.recognizedPoints(.all)[.wrist] else {
+            return false
+        }
+        
+
+        guard let wristPoints = try? observation.recognizedPoints(.all),
+              let wrist = wristPoints[.wrist] else {
+            return false
+        }
+
+        let confidenceThreshold: Float = 0.3
+        let extendedDistanceThreshold: CGFloat = 0.20
+        
+        func isFingerExtended(fingerTip: VNRecognizedPoint?) -> Bool {
+            guard let tip = fingerTip, tip.confidence > confidenceThreshold else {
+                return false
+            }
+            let dis = distance(from: tip.location, to: wrist.location)
+            print(dis)
+            return dis > extendedDistanceThreshold
+        }
+
+        let thumbExtended = isFingerExtended(fingerTip: thumbPoints[.thumbTip])
+        let indexExtended = isFingerExtended(fingerTip: indexPoints[.indexTip])
+        let middleExtended = isFingerExtended(fingerTip: middlePoints[.middleTip])
+        let ringExtended = isFingerExtended(fingerTip: ringPoints[.ringTip])
+        let littleExtended = isFingerExtended(fingerTip: littlePoints[.littleTip])
+        
+//        print("Thumb Points:", thumbExtended)
+//        print("Index Points:", indexExtended)
+//        print("Middle Points:", middleExtended)
+//        print("Ring Points:", ringExtended)
+//        print("Little Points:", littleExtended)
+
+        // Victory position is inferred if only the index and middle fingers are extended
+        let victoryDetected = indexExtended && middleExtended && !thumbExtended && !ringExtended && !littleExtended
+        print(victoryDetected)
+        return victoryDetected
     }
 
 }
