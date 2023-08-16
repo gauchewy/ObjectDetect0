919ab2b63a437cd98f2e2235fa742c47d5cd8797
gauchewy <90925919+gauchewy@users.noreply.github.com>
2023-08-16 13:01:20 -0400

    ZStack order changed, just need buffer

--- ObjectDetect0/ContentView.swift
+++ ObjectDetect0/ContentView.swift
@@ -39,75 +39,74 @@ struct ContentView: View {
 
 
     var body: some View {
-        
-        ZStack{
-            ZStack(alignment: .bottomTrailing) {
-                VStack {
-                    Image("abe")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                    Spacer()
-                }
+        ZStack {
+            Image("abe")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
                 .padding()
-                
-                
-                if isTwoHandsDetected {
-                    Rectangle()
-                        .fill(Color.clear)
-                    
-                } else {
-                    Rectangle()
-                        .fill(Color.black)
-                }
-                
-                Spacer()
-                
-                CameraView()
-                    .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 4)
-                    .background((selectedOption == 0 && isTwoHandsDetected) || (selectedOption == 1 && isVictoryPoseDetected) ? Color.clear : Color.black)
-                    .cornerRadius(20)
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.green, lineWidth: (selectedOption == 0 && isTwoHandsDetected) || (selectedOption == 1 && isVictoryPoseDetected) ? 5 : 0))
-                    .padding()
 
-            } //edit here
-            .onReceive(NotificationCenter.default.publisher(for: .numberOfHandsDetectedChanged)) { notification in
-                if selectedOption == 0, let numberOfHands = notification.object as? Int {
-                    isTwoHandsDetected = numberOfHands == 2
-                }
-            }
-            .onReceive(NotificationCenter.default.publisher(for: .numberOfVictoryHandsDetectedChanged)) { notification in
-                if selectedOption == 1, let numberOfVictoryHands = notification.object as? Int {
-                    isVictoryPoseDetected = numberOfVictoryHands == 2
-                }
+            // Conditions to display the black Rectangle based on option selected
+            if (selectedOption == 0 && !isTwoHandsDetected) || (selectedOption == 1 && !isVictoryPoseDetected) {
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
 
-            
             VStack {
-                           
+                Spacer()
+
+                HStack {
+                    Spacer()
+
+                    CameraView()
+                        .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 4)
+                        .background(
+                            (selectedOption == 0 && isTwoHandsDetected) || (selectedOption == 1 && isVictoryPoseDetected) ? Color.clear : Color.black
+                        )
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20).stroke(Color.green, lineWidth: (selectedOption == 0 && isTwoHandsDetected) || (selectedOption == 1 && isVictoryPoseDetected) ? 5 : 0)
+                        )
+                        .padding()
+                }
+            }
+
+            VStack {
                 Menu(menuTitle) {
                     Button(action: { selectedOption = 0 }) {
                         Text("Two Hands")
-                            .font(.system(size: 18, weight: .bold)) // Larger and bold text
+                            .font(.system(size: 18, weight: .bold))
                     }
                     Button(action: { selectedOption = 1 }) {
                         Text("Victory Pose")
-                            .font(.system(size: 18, weight: .bold)) // Larger and bold text
+                            .font(.system(size: 18, weight: .bold))
                     }
                     Button(action: { selectedOption = 2 }) {
                         Text("Option 3")
-                            .font(.system(size: 18, weight: .bold)) // Larger and bold text
+                            .font(.system(size: 18, weight: .bold))
                     }
                 }
-
                 .padding()
                 .foregroundColor(Color.black)
                 .background(Color.white)
                 .cornerRadius(10)
                 .padding(.top, 20)
+
                 Spacer()
             }
         }
+        .onReceive(NotificationCenter.default.publisher(for: .numberOfHandsDetectedChanged)) { notification in
+            if selectedOption == 0, let numberOfHands = notification.object as? Int {
+                isTwoHandsDetected = numberOfHands == 2
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: .numberOfVictoryHandsDetectedChanged)) { notification in
+            if selectedOption == 1, let numberOfVictoryHands = notification.object as? Int {
+                isVictoryPoseDetected = numberOfVictoryHands == 2
+            }
+        }
     }
+
 }
 
 
